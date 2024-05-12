3fbe0e7d6ae1dbf4fefe764d1fa45624c2c9eafc
code-srikar <official.srikar.work@gmail.com>
2024-05-12 18:29:48 +0530

    Signing

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import React from "react";
+import { signOut } from "firebase/auth";
+import { database } from "./FirebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+
+    const history = useNavigate();
+
+    const handleClick = () => {
+        signOut(database)
+            .then(
+                history('/')
+            )
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
+            <button onClick={handleClick}>Sign Out</button>
         </div>
     )
 }
--- src/signup/Signup.jsx
+++ src/signup/Signup.jsx
@@ -1,12 +1,14 @@
 import './signup.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { database } from '../FirebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
 
     const history = useNavigate();
+
+    const [login, setLogin] = useState(false);
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
@@ -14,23 +16,36 @@ function SignUp() {
         const password = e.target.password.value;
         const uname = e.target.uname.value;
 
-        createUserWithEmailAndPassword(database, email, password)
-            .then(
-                data => {
-                    console.log(data);
-                    history('../Home');
-                }
-            )
-            .catch(
-                err => {
-                    alert(err.message);
-                }
-            )
+        if (type == 'signup') {
+            createUserWithEmailAndPassword(database, email, password)
+                .then(
+                    data => {
+                        console.log(data);
+                        history('../Home');
+                    }
+                )
+                .catch(
+                    err => {
+                        alert(err.message);
+                    }
+                )
+        }
+        else {
+            signInWithEmailAndPassword(database, email, password)
+                .then(
+                    history('/home')
+                )
+                .catch(
+                    err => {
+                        alert(err.message);
+                    }
+                )
+        }
     }
 
     return <div className='container bg-dark'>
-        <h1 id='title' style={{ color: 'white' }}>Register Here</h1>
-        <form onSubmit={(e) => handleOnSubmit(e)}>
+        <h1 id='title' style={{ color: 'white' }}>{login ? 'Sign In' : 'Sign Up'}</h1>
+        <form onSubmit={(e) => handleOnSubmit(e, login ? 'signin' : 'signup')}>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Username</span>
                 <input type="text" id='uname' class="form-control" placeholder="Enter Your Username" aria-label="Username" aria-describedby="basic-addon1" />
@@ -43,7 +58,7 @@ function SignUp() {
                 <span class="input-group-text" id="basic-addon1">Password</span>
                 <input type="password" id='password' class="form-control" placeholder="Enter Your Password" aria-label="Username" aria-describedby="basic-addon1" />
             </div>
-            <input class="btn btn-primary" type="submit" value="Submit"></input>
+            <input class="btn btn-primary" type="submit">{login ? 'Sign In' : 'Sign Up'}</input>
         </form>
     </div>
 }
